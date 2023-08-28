7bb2f21396519221bef673274927240011196cbb
Grant Goodyear <grant.goodyear@bakerhughes.com>
2023-08-28 09:22:06 -0500

    add rust and latex "extras"

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
     { import = "plugins" },
